442d35f0870d720d6206fc0c9d0c1b684d63ee07
devfag <devfag@protonmail.com>
2020-01-05 17:01:30 -0500

    Revert person mapping functions to prevent issue #43 from recurring

--- utils.py
+++ utils.py
@@ -616,22 +616,24 @@ def standardize_punctuation(text):
 
 
 def first_to_second_person(text):
+    text = " " + text
     text = standardize_punctuation(text)
+    if text[-1] not in [".", "?", "!"]:
+        text += "."
     for pair in first_to_second_mappings:
         variations = mapping_variation_pairs(pair)
         for variation in variations:
             text = replace_outside_quotes(text, variation[0], variation[1])
-    if text[-1] not in [".", "?", "!"]:
-        text += "."
     return text
 
 
 def second_to_first_person(text):
+    text = " " + text
     text = standardize_punctuation(text)
+    if text[-1] not in [".", "?", "!"]:
+        text += "."
     for pair in second_to_first_mappings:
         variations = mapping_variation_pairs(pair)
         for variation in variations:
             text = replace_outside_quotes(text, variation[0], variation[1])
-    if text[-1] not in [".", "?", "!"]:
-        text += "."
     return text
